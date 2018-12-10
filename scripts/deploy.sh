16ee28815fd13f95e78f86de5f507aac05f21c01
chungyeol <loyalten109@gmail.com>
2018-12-10 17:34:33 +0900

    commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,6 @@ BUILD_PATH=$(ls /home/ec2-user/build/target/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
 
-echo "> build 파일 복사"
 DEPLOY_PATH=/home/ec2-user/deploy/
 cp $BUILD_PATH $DEPLOY_PATH
 
